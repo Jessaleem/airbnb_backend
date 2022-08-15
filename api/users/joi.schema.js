76cde8a6ed2a4404a70886a6ff7bf0cb16f592be
adrianvilla1504 <adriancvilla@gmail.com>
2022-08-14 19:31:30 -0500

    feat: start joi schemas

---
+++ api/users/joi.schema.js
@@ -0,0 +1,49 @@
+const Joi = require('joi');
+
+const today = Date.now();
+const cutoffDate = new Date(today - (18 * 365 * 24 * 60 * 60 * 1000));
+
+const schema = Joi.object({
+  name:
+    Joi.string()
+      .required(),
+  lastName:
+    Joi.string()
+      .required(),
+  phone:
+    Joi.string()
+      .required(),
+  email:
+    Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required()
+      .case('lower'),
+  gender:
+    Joi.string()
+      .default('other'),
+  about:
+    Joi.string()
+      .required(),
+  avatar:
+    Joi.string()
+      .default('https://res.cloudinary.com/equipo-maravilla/image/upload/v1659716563/images/Account/Userlogo_pyxlip.png'),
+  role:
+    Joi.string()
+      .default('guest')
+      .valid('guest', 'host', 'admin')
+      .required(),
+  isActive:
+    Joi.boolean()
+      .default(false),
+  birthDate:
+    Joi.date()
+      .min(cutoffDate)
+      .required(),
+  password:
+    Joi.string()
+      .alphanum()
+      .min(8)
+      .required(),
+});
+
+module.exports = schema;
