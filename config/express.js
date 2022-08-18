2f7bae183112e0ae213e69086e7fbd50d523c1da
adrianvilla1504 <adriancvilla@gmail.com>
2022-08-17 20:37:33 -0500

    feat: add cors on backend

--- config/express.js
+++ config/express.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
 function configExpress(app) {
+  app.use(cors());
   app.use(express.json());
   app.use(morgan('dev'));
 }
