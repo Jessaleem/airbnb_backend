0b4cb772959f61d35601723734252e57b073592b
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-21 12:34:54 -0500

    feat: implement sendgrid (has bugs)

--- auth/local/index.js
+++ auth/local/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const {
   loginUserHandler,
+  verifyAccountHandler,
 } = require('./auth.controller');
 
 const router = Router();
 
 router.post('/login', loginUserHandler);
+router.get('/verifyAccount/:token', verifyAccountHandler);
 
 module.exports = router;
