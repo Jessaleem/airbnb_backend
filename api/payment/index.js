c5161cf19f08854e8a64404f004756ca545efa34
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-27 18:59:46 -0500

    feat: add authorization by token and role (role did not work

--- api/payment/index.js
+++ api/payment/index.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 
 const handlerPayment = require('./payment.controller');
-// const { isAuthenticated, verifyRole } = require('../../auth/auth.service');
-// ----> no me autoriza cuando aun siendo GUEST
+const { isAuthenticated /* verifyRole */ } = require('../../auth/auth.service');
+// ----> no me autoriza siendo GUEST o ADMIN
 
 const router = Router();
 
-router.post('/', handlerPayment);
+router.post('/', isAuthenticated, handlerPayment);
 
 module.exports = router;
