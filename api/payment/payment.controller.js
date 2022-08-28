34a5e186c88c13c295e7d16fc905391fe5ec6d42
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-27 19:00:15 -0500

    feat: add services for payments

--- api/payment/payment.controller.js
+++ api/payment/payment.controller.js
@@ -1,20 +1,61 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 const {
   makePayment,
   createPayment,
+  createCustomer,
+  retrieveCustomer,
 } = require('./payment.service');
+const { updateUser } = require('../users/users.services');
 
 async function handlerPayment(req, res) {
   const { user } = req;
   const { paymentMethod, amount } = req.body;
-  if (!user?.payment?.customerId) {
-    res.json({ message: 'Create new user' });
-    return;
-  }
+
   try {
-    res.json({ amount, user });
+    const { id, card } = paymentMethod;
+
+    let customer = null;
+    if (!user?.payment?.customerId) {
+      customer = await createCustomer(user, paymentMethod);
+
+      const userToUpdate = {
+        payment: {
+          customerId: customer.id,
+          cards: [{
+            paymentMethodId: id,
+            brand: card.brand,
+            country: card.country,
+            expMonth: card.exp_month,
+            expYear: card.exp_year,
+            funding: card.funding,
+            last4: card.last4,
+          }],
+        },
+      };
+      await updateUser(user._id, userToUpdate);
+    }
+
+    customer = await retrieveCustomer(user.payment.customerId);
+
+    const paymentCard = {
+      id: user.payment.cards[0].paymentMethodId,
+    };
+
+    const payment = await makePayment({ paymentMethod: paymentCard, amount, customer });
+    const registerPayment = {
+      refId: payment.id,
+      description: payment.description,
+      value: payment.amount,
+      currency: payment.currency,
+      userId: user._id,
+    };
+
+    await createPayment(registerPayment);
+
+    return res.json(payment);
   } catch (error) {
-    res.status(500).json({ '[ERROR': error.message });
+    res.status(500).json({ '[ERROR]': error.message });
+    throw error;
   }
 }
 
