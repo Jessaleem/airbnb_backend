5938e7d73d2ffeca3534a62d0a731592d31a297c
Jessica <aleejess@gmail.com>
2022-09-05 19:39:14 -0500

    feat: add actual password confirmation

--- auth/local/auth.controller.js
+++ auth/local/auth.controller.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcryptjs');
-const { findUserByEmail, findOneUser } = require('../../api/users/users.services');
+const {
+  findUserByEmail,
+  findOneUser,
+  getSingleUser,
+  updateUser,
+} = require('../../api/users/users.services');
 const { signToken } = require('../auth.service');
-const User = require('../../api/users/users.model');
 
 async function loginUserHandler(req, res) {
   const { email, password } = req.body;
@@ -57,9 +61,27 @@ async function verifyAccountHandler(req, res) {
 async function changePassword(req, res) {
   try {
     const { id } = req.params;
+    const { oldPassword } = req.body;
+    const newPassword = req.body.password;
+
+    const user = await getSingleUser({ _id: id });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Password incorrect' });
+    }
+    const isMatch = await user.comparePassword(oldPassword);
+
+    if (!isMatch) {
+      return res.status(404).json({ message: 'Password incorrect' });
+    }
+
+    if (oldPassword === newPassword) {
+      return res.status(400).json({ message: 'New password not allowed' });
+    }
+
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
-    const userPassword = await User.findByIdAndUpdate({ _id: id }, { password }, { new: true });
+    const userPassword = await updateUser({ _id: id }, { password }, { new: true });
     return res.status(200).json({ status: true, data: userPassword });
   } catch (error) {
     return res.status(400).json({ status: false, error: 'Error Occured' });
